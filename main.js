6bf5b3ef797d309c660839e90bb84018d4220d28
AMosab <mosab.atchane@synaptique.com>
2016-06-30 12:30:48 +0000

    fixes

--- main.js
+++ main.js
@@ -4,38 +4,44 @@ var sender_email;
 var data_id;
 var data;
 var email_plain_body;
-
+var rootAppName = "https://www.localapplicant.com/"
+var availableTags = [
+  "ActionScript",
+  "AppleScript",
+  "Asp"
+];
 
 function getRfqKeywords() {
-    var _url = rootAppName + "RFQ/getRFQKeywords";
-    var rfqKeywordList;
-    $.ajax({
-        type: 'post',
-        url: _url,
-        async: false,
-        success: function (jsonResponse) {
-            rfqKeywordList = jsonResponse;
-        }
-    });
-    return rfqKeywordList;
+  var _url = rootAppName + "RFQ/getRFQKeywords";
+  var rfqKeywordList;
+
+  $.ajax({
+    type: 'post',
+    url: _url,
+    data: "",
+
+    async: false,
+    success: function (jsonResponse) {
+      rfqKeywordList = jsonResponse;
+    }
+  });
+  return rfqKeywordList;
 }
 
 function addAutocompleteToSearch() {
-    var rfqKeywordList = getRfqKeywords();
-    $('#rfqFilterSearch').autocompleter({
-        highlightMatches: true,
-        source: rfqKeywordList,
-        template: '{{ label }}',
-        empty: false,
-        limit: 10,
-        callback: function (value, index, selected) {
-            if (selected) {
-                $('#rfqFilterSearch').val(selected.label);
-                $('#hiddenSearchInput').val(selected.id);
-                searchAllRfqsAndOrders();
-            }
-        }
-    });
+  var rfqKeywordList = getRfqKeywords();
+  $('#rfqFilterSearch').autocompleter({
+    highlightMatches: true,
+    source: rfqKeywordList,
+    template: '{{ label }}',
+    empty: false,
+    limit: 10,
+    callback: function (value, index, selected) {
+      if (selected) {
+
+      }
+    }
+  });
 }
 
 
@@ -72,18 +78,12 @@ var main = function () {
 
     $('.hi').append('<div class="nH hh"><iframe src="https://www.localapplicant.com/dashboard"></iframe></div>');
 
-    $('.hi').append('<div class="nH hh"><div class="c0"><div class="cV"><div class="cX"><img class="cY" src="http://cluster006.ovh.net/~synaptiq/synaptique/images/logo-syn.png" height="16"><span class="cZ">Synaptique extension_test</span><span class="cU"> - Stay up to date!</span>'
-      + '</br></br><span class="cU">Hi ' + gmail.get.user_email() + ' </br></br><button type="button">Sing in</button></span></div><div class="cT"></div></div><div></div></div></div>');
-    $('.hi').append("<div class=\"ui-widget\"><label for=\"Item Id\">Ids: </label><input id=\"rfqFilterSearch\"></div>");
+    /*$('.hi').append('<div class="nH hh"><div class="c0"><div class="cV"><div class="cX"><img class="cY" src="http://cluster006.ovh.net/~synaptiq/synaptique/images/logo-syn.png" height="16"><span class="cZ">Synaptique extension_test</span><span class="cU"> - Stay up to date!</span>'
+      + '</br></br><span class="cU">Hi ' + gmail.get.user_email() + ' </br></br><button type="button">Sing in</button></span></div><div class="cT"></div></div><div></div></div></div>');*/
+    $('.hi').append("<div class=\"ui-widget\"><label for=\"Item Id\">Search: </label><input id=\"rfqFilterSearch\"></div>");
 
-    var availableTags = [
-      "ActionScript",
-      "AppleScript",
-      "Asp"
-    ];
-    $("#rfqFilterSearch").autocomplete({
-      source: availableTags
-    });
+
+    addAutocompleteToSearch();
   });
 }
 
