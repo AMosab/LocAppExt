b2877fe0eed141c2afbc91eda1e3795dd92a81f2
AMosab <mosab.atchane@synaptique.com>
2016-07-05 15:16:36 +0000

    commented alpha

--- main.js
+++ main.js
@@ -4,38 +4,43 @@ var sender_email;
 var data_id;
 var data;
 var email_plain_body;
-var rootAppName = "https://www.localapplicant.com/"
+var rootAppName = "https://www.localapplicant.com/"//staging.synaptique.com
 var availableTags = [
   "ActionScript",
   "AppleScript",
   "Asp"
 ];
 
+var isdebug = false;
 
+//ease changing between developement modes
 function bug(isdebug) {
   if (isdebug) {
-    debugger
+    bug(isdebug);
   }
-  else
-  {
-    
+  else {
+    //todo
   }
 }
-function iframeLoaded() {
-  debugger
-      var iFrameID = document.getElementById('Iframe_synap');
-      if(iFrameID) {
-            // here you can make the height, I delete it first, then I make it again
-                        console.log(iFrameID.height)
-debugger
-            iFrameID.height = "";
-            console.log(iFrameID.height)
-            iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
-            console.log(iFrameID.height)
-            
-      }   
-  }
 
+
+//adjust the iframe height after done loading
+function iframeLoaded() {
+  bug(isdebug);
+  var iFrameID = document.getElementById('Iframe_synap');
+  if (iFrameID) {
+    // here you can make the height, I delete it first, then I make it again
+    console.log(iFrameID.height)
+    bug(isdebug);
+    iFrameID.height = "";
+    console.log(iFrameID.height)
+    iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
+    console.log(iFrameID.height)
+
+  }
+}
+
+//fetch the Keywords list from the server
 function getRfqKeywords() {
   var _url = rootAppName + "RFQ/getRFQKeywords";
   var rfqKeywordList;
@@ -59,8 +64,10 @@ function getRfqKeywords() {
   return rfqKeywordList;
 }
 
+
+
+
 function addAutocompleteToSearch() {
-  var rfqKeywordList = getRfqKeywords();
   var filtered = [];
   json.forEach(function (item) {
     console.log(item);
@@ -72,26 +79,34 @@ function addAutocompleteToSearch() {
     return el
   });
   console.log(arr);
-  debugger
+  bug(isdebug);
 
+  var rfqKeywordList = arr;
+  //var rfqKeywordList = getRfqKeywords();//uncomment this when working online(not locally)
 
-  $('#rfqFilterSearch').autocompleter({
-    source: arr,
+  $('#rfqFilterSearch').autocompleter({ //jquery autocompleter
+    source: rfqKeywordList,
+    highlightMatches: true,
+    hint: true,
+    empty: false,
+    limit: 10,
 
     callback: function (value, index, selected) {
-      debugger
+      bug(isdebug);
       if (selected) {
         console.log(selected.label);
         console.log(selected.id);
-        //showUpdateModalSingleItem(selected.id);https://www.localapplicant.com/item/getOrderDetailModal?id=it227
-    //$('.hi_synap').html('<div class="nH hh"><iframe id="Iframe_synap" src="https://www.localapplicant.com/item/getOrderDetailModal?id=it227" onload="iframeLoaded()"></iframe></div>');
-$('.hi_synap').html(fmodal);
+        //showUpdateModal(selected.id);https://www.localapplicant.com/item/getOrderDetailModal?id=it227
+        //$('.hi_synap').html('<div class="nH hh"><iframe id="Iframe_synap" src="https://www.localapplicant.com/item/getOrderDetailModal?id=it227" onload="iframeLoaded()"></iframe></div>');
+        $('.hi_synap_i').html(fmodal);
       }
     }
   });
 }
 
-function showUpdateModalSingleItem(id) {
+
+//fetch the update Modal and desplay it
+function showUpdateModal(id) {
   $('.overlay-ajax').show();
   var url_ = rootAppName + "item/getOrderDetailModal";
   $.ajax({
@@ -116,6 +131,8 @@ function showUpdateModalSingleItem(id) {
     }
   });
 }
+
+//keep refreshing until the webpage done loading
 function refresh(f) {
   if ((/in/.test(document.readyState)) || (typeof Gmail === undefined)) {
     setTimeout('refresh(' + f + ')', 10);
@@ -124,12 +141,16 @@ function refresh(f) {
   }
 }
 
+
+
 var main = function () {
 
   gmail = new Gmail();
 
-
+//excute each time the user open an email
   gmail.observe.on("open_email", function (id, url, body, xhr) {
+
+    
     function reqListener() {
       console.log(this.responseText);
       console.log(this.status);
@@ -141,7 +162,7 @@ var main = function () {
     oReq.addEventListener("load", reqListener);
     //oReq.withCredentials = true;
     oReq.open("POST", "https://api.predic8.de/shop/");
-    oReq.send();
+    //oReq.send();
 
     data_id = gmail.get.email_data(id);
     email_subject = data_id.subject;
@@ -154,12 +175,13 @@ var main = function () {
     $('.hi').append('<iframe width="0" height="0" border="0" name="dummyframe" id="dummyframe"></iframe>');
 
     $('.hi').append('<div class="nH hh"><div class="c0"><div class="cV"><div class="cX"><img class="cY" src="http://cluster006.ovh.net/~synaptiq/synaptique/images/logo-syn.png" height="16"><span class="cZ">Synaptique extension_test</span><span class="cU"> - Stay up to date!</span>'
-      + '</br></br></div><div class="cT"></div></div><div></div><div class=\"hi_synap\"><label for=\"Item Id\">Search: </label><input id=\"rfqFilterSearch\"></div></div></div>');
-    $('.hi').append("<form ACTION=\"http://www.cs.tut.fi/cgi-bin/run/~jkorpela/echo.cgi\" METHOD=\"POST\" target=\"dummyframe\"><div class=\"hi_synap\"></div>  </br><input type=\"submit\" value=\"Submit\"></form>");
+      + '</br></br></div><div class="cT"></div></div><div></div><div class=\"hi_synap\"><label for=\"rfqFilterSearch\">Search for an Item: </label></br><input id=\"rfqFilterSearch\"></div></div></div>');
+    $('.hi').append("<form ACTION=\"http://www.cs.tut.fi/cgi-bin/run/~jkorpela/echo.cgi\" METHOD=\"POST\" target=\"dummyframe\"><div class=\"hi_synap_i\"></div></form>");
 
 
     addAutocompleteToSearch();
   });
 }
 
+//the trigger(the first line to get excuted)
 refresh(main);
